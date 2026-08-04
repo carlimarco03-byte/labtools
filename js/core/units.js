87f4dcc1682696b5a75621569c8c3926226fbfd8
carlimarco03-byte <carli.marco03@gmail.com>
2026-08-04 16:36:47 +0200

    Update units.js

--- js/core/units.js
+++ js/core/units.js
@@ -1,1 +1,19 @@
+function convertMolarity(value, unit){
 
+    switch(unit){
+
+        case "M":
+            return value;
+
+        case "mM":
+            return value / 1000;
+
+        case "uM":
+            return value / 1000000;
+
+        default:
+            return value;
+
+    }
+
+}
