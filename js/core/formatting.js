e7d3adb8f45239936c9372dc437a307c18b23b17
carlimarco03-byte <carli.marco03@gmail.com>
2026-08-04 16:36:27 +0200

    Update formatting.js

--- js/core/formatting.js
+++ js/core/formatting.js
@@ -1,1 +1,29 @@
+function formatMass(mass){
 
+    if(mass >= 1){
+
+        return `
+            ${mass.toFixed(3)} g
+            <br><small>(${(mass*1000).toFixed(1)} mg)</small>
+        `;
+
+    }
+
+    else if(mass >= 0.001){
+
+        return `
+            ${(mass*1000).toFixed(3)} mg
+            <br><small>(${(mass*1000000).toFixed(1)} µg)</small>
+        `;
+
+    }
+
+    else{
+
+        return `
+            ${(mass*1000000).toFixed(2)} µg
+        `;
+
+    }
+
+}
