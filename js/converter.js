c0bec13256b029ad948847c196e2313850326ecd
carlimarco03-byte <carli.marco03@gmail.com>
2026-08-05 12:01:09 +0200

    Update converter.js

--- js/converter.js
+++ js/converter.js
@@ -1,3 +1,10 @@
+function convert(){
+
+    document.getElementById("result").innerHTML =
+    "Conversion engine coming in the next step...";
+
+}
+
 document.addEventListener("DOMContentLoaded", function(){
 
     displayHistory();
