3114fe04ac3d803434be40c529b937ea1e2d952a
carlimarco03-byte <carli.marco03@gmail.com>
2026-08-06 12:21:27 +0200

    Update engine.js

--- js/reaction-builder/engine.js
+++ js/reaction-builder/engine.js
@@ -1,5 +1,125 @@
 function calculateVolumes(){
 
-    console.log("Engine running");
+
+    let usedVolume = 0;
+
+
+    components.forEach(component => {
+
+
+        if(component.method === "concentration"){
+
+
+            let volume =
+            (
+                component.target *
+                reactionSettings.volume
+            )
+            /
+            component.stock;
+
+
+            component.volumeReaction =
+            volume;
+
+
+        }
+
+
+        else if(component.method === "fixedVolume"){
+
+
+            component.volumeReaction =
+            component.volume || 0;
+
+
+        }
+
+
+        else if(component.method === "auto"){
+
+
+            component.volumeReaction = 0;
+
+
+        }
+
+
+
+        if(component.type !== "water"){
+
+            usedVolume += component.volumeReaction;
+
+        }
+
+
+    });
+
+
+
+    components.forEach(component => {
+
+
+        if(component.type === "water"){
+
+
+            component.volumeReaction =
+            reactionSettings.volume - usedVolume;
+
+
+        }
+
+
+    });
+
+
+
+    calculateMasterMix();
+
+
+}
+
+function calculateMasterMix(){
+
+
+    let totalReactions =
+
+    reactionSettings.reactions
+    +
+    reactionSettings.extra;
+
+
+
+    totalReactions =
+    totalReactions *
+    (1 + reactionSettings.overage / 100);
+
+
+
+    components.forEach(component => {
+
+
+        if(component.includeMM){
+
+
+            component.volumeMasterMix =
+
+            component.volumeReaction *
+            totalReactions;
+
+
+        }
+
+        else {
+
+
+            component.volumeMasterMix = 0;
+
+
+        }
+
+
+    });
+
 
 }
