4dd3646dbe97a1c8ebaa63534dfb926a4a9b1d97
carlimarco03-byte <carli.marco03@gmail.com>
2026-08-06 10:41:33 +0200

    Update renderer.js

--- js/reaction-builder/renderer.js
+++ js/reaction-builder/renderer.js
@@ -8,17 +8,88 @@ function renderComponents() {
 
         area.innerHTML += `
 
-        <div class="component-card">
+<div class="component-card">
+
+    <div class="component-header">
+
+        <div>
 
             <h3>${component.name}</h3>
 
-            <p>Type: ${component.type}</p>
-
-            <p>Method: ${component.method}</p>
+            <small>${component.type}</small>
 
         </div>
 
-        `;
+        <button class="menu-btn">
+
+            ⋮
+
+        </button>
+
+    </div>
+
+    <div class="component-body">
+
+        <div class="form-group">
+
+            <label>Method</label>
+
+            <input
+            type="text"
+            value="${component.method}"
+            disabled>
+
+        </div>
+
+        <div class="form-group">
+
+            <label>Stock</label>
+
+            <input
+            type="text"
+            value="${component.stock ?? "-"} ${component.stockUnit ?? ""}"
+            disabled>
+
+        </div>
+
+        <div class="form-group">
+
+            <label>Target</label>
+
+            <input
+            type="text"
+            value="${component.target ?? "-"} ${component.targetUnit ?? ""}"
+            disabled>
+
+        </div>
+
+        <div class="form-group">
+
+            <label>Calculated Volume</label>
+
+            <input
+            type="text"
+            value="AUTO"
+            disabled>
+
+        </div>
+
+        <label>
+
+            <input
+            type="checkbox"
+            ${component.includeMM ? "checked" : ""}
+            disabled>
+
+            Include in Master Mix
+
+        </label>
+
+    </div>
+
+</div>
+
+`;
 
     });
 
