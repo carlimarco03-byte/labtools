de8cc39f5d180f717a5aa36b1ae74aec549fa8da
carlimarco03-byte <carli.marco03@gmail.com>
2026-08-06 12:11:45 +0200

    Update templates.js

--- js/reaction-builder/templates.js
+++ js/reaction-builder/templates.js
@@ -20,27 +20,39 @@ const PCR_TEMPLATE = [
 },
 
 {
-    id: 2,
-    name: "MgCl₂",
-    type: "salt",
-    method: "concentration",
-    stock: 25,
-    stockUnit: "mM",
-    target: 2,
-    targetUnit: "mM",
-    includeMM: true
+    id:2,
+    name:"Forward Primer",
+    type:"primer",
+    method:"concentration",
+
+    stock:10,
+    stockUnit:"µM",
+
+    target:0.5,
+    targetUnit:"µM",
+
+    volumeReaction:0,
+    volumeMasterMix:0,
+
+    includeMM:true
 },
 
 {
-    id: 3,
-    name: "Forward Primer",
-    type: "primer",
-    method: "concentration",
-    stock: 10,
-    stockUnit: "µM",
-    target: 0.5,
-    targetUnit: "µM",
-    includeMM: true
+    id:3,
+    name:"DNA",
+    type:"template",
+    method:"fixedVolume",
+
+    stock:20,
+    stockUnit:"ng/µL",
+
+    target:null,
+    targetUnit:null,
+
+    volumeReaction:0,
+    volumeMasterMix:0,
+
+    includeMM:false
 },
 
 {
@@ -52,6 +64,8 @@ const PCR_TEMPLATE = [
     stockUnit: "µM",
     target: 0.5,
     targetUnit: "µM",
+     volumeReaction:0,
+    volumeMasterMix:0,
     includeMM: true
 },
 
