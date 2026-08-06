de41b1a4cff940331b03ea09d604bec86fbaaee4
carlimarco03-byte <carli.marco03@gmail.com>
2026-08-06 10:33:53 +0200

    Update renderer.js

--- js/reaction-builder/renderer.js
+++ js/reaction-builder/renderer.js
@@ -1,1 +1,25 @@
+function renderComponents() {
 
+    const area = document.getElementById("calculatorArea");
+
+    area.innerHTML = "";
+
+    components.forEach(component => {
+
+        area.innerHTML += `
+
+        <div class="component-card">
+
+            <h3>${component.name}</h3>
+
+            <p>Type: ${component.type}</p>
+
+            <p>Method: ${component.method}</p>
+
+        </div>
+
+        `;
+
+    });
+
+}
