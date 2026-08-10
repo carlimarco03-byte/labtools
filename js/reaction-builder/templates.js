d3b0bc674994be615564e577d442185133d3b4a8
carlimarco03-byte <carli.marco03@gmail.com>
2026-08-10 19:31:08 +0200

    Update templates.js

--- js/reaction-builder/templates.js
+++ js/reaction-builder/templates.js
@@ -147,7 +147,135 @@ const REACTION_TEMPLATES = {
 
     "PCR": PCR_TEMPLATE,
 
-    "qPCR (SYBR)": [],
+    "qPCR (SYBR)": [
+
+    {
+        id:101,
+
+        name:"Water",
+
+        type:"water",
+
+        method:"auto",
+
+        locked:true,
+
+        order:1,
+
+        volumeReaction:0,
+
+        volumeMasterMix:0,
+
+        includeMM:true
+    },
+
+    {
+        id:102,
+
+        name:"2X SYBR Master Mix",
+
+        type:"mastermix",
+
+        method:"concentration",
+
+        order:2,
+
+        stock:2,
+
+        stockUnit:"X",
+
+        target:1,
+
+        targetUnit:"X",
+
+        volumeReaction:0,
+
+        volumeMasterMix:0,
+
+        includeMM:true
+    },
+
+    {
+        id:103,
+
+        name:"Forward Primer",
+
+        type:"primer",
+
+        method:"concentration",
+
+        order:3,
+
+        stock:10,
+
+        stockUnit:"µM",
+
+        target:0.5,
+
+        targetUnit:"µM",
+
+        volumeReaction:0,
+
+        volumeMasterMix:0,
+
+        includeMM:true
+    },
+
+    {
+        id:104,
+
+        name:"Reverse Primer",
+
+        type:"primer",
+
+        method:"concentration",
+
+        order:4,
+
+        stock:10,
+
+        stockUnit:"µM",
+
+        target:0.5,
+
+        targetUnit:"µM",
+
+        volumeReaction:0,
+
+        volumeMasterMix:0,
+
+        includeMM:true
+    },
+
+    {
+        id:105,
+
+        name:"DNA",
+
+        type:"template",
+
+        method:"fixedVolume",
+
+        order:5,
+
+        stock:20,
+
+        stockUnit:"ng/µL",
+
+        volume:2,
+
+        target:null,
+
+        targetUnit:null,
+
+        volumeReaction:0,
+
+        volumeMasterMix:0,
+
+        includeMM:false
+    }
+
+],
 
     "qPCR (Probe)": [],
 
